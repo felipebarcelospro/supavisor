be9b5292452b48d0f5e60f7e4d98e0c649c4786b
Felipe Barcelos <felipe@getfeed.com.br>
2024-01-17 08:52:17 -0300

    Update 00-setup.sql 3

--- dev/postgres/00-setup.sql
+++ dev/postgres/00-setup.sql
@@ -1,11 +1,19 @@
+-- Script para criar roles e conceder privilégios
+-- Nomeie este arquivo para garantir que seja o primeiro a ser executado, por exemplo, "00-initial-setup.sql"
+
 DO $$
-BEGIN    
-    -- Verifica e cria o role 'authenticated' se não existir
+BEGIN
+    -- Cria o role 'anon' se ele não existir
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'anon') THEN
+        CREATE ROLE anon NOLOGIN NOINHERIT;
+    END IF;
+    
+    -- Cria o role 'authenticated' se ele não existir
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticated') THEN
         CREATE ROLE authenticated NOLOGIN NOINHERIT;
     END IF;
 
-    -- Verifica e cria o role 'service_role' se não existir
+    -- Cria o role 'service_role' se ele não existir
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'service_role') THEN
         CREATE ROLE service_role NOLOGIN NOINHERIT BYPASSRLS;
     END IF;

17f92a9eab8ef3d974a60c489a3a674d79ba2f1d
Felipe Barcelos <felipe@getfeed.com.br>
2024-01-17 08:47:06 -0300

    Update 00-setup.sql

--- dev/postgres/00-setup.sql
+++ dev/postgres/00-setup.sql
@@ -1,11 +1,26 @@
-create role anon          nologin noinherit;
-create role authenticated nologin noinherit;
-create role service_role  nologin noinherit bypassrls;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'anon') THEN
+        CREATE ROLE anon NOLOGIN NOINHERIT;
+    END IF;
+    
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'authenticated') THEN
+        CREATE ROLE authenticated NOLOGIN NOINHERIT;
+    END IF;
 
-grant usage on schema public to anon, authenticated, service_role;
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'service_role') THEN
+        CREATE ROLE service_role NOLOGIN NOINHERIT BYPASSRLS;
+    END IF;
+END
+$$;
 
-alter default privileges in schema public grant all on tables    to anon, authenticated, service_role;
-alter default privileges in schema public grant all on functions to anon, authenticated, service_role;
-alter default privileges in schema public grant all on sequences to anon, authenticated, service_role;
+-- Concede privilégios básicos para os roles
+GRANT USAGE ON SCHEMA public TO anon, authenticated, service_role;
 
-create schema if not exists _supavisor;
+-- Altera os privilégios padrões para tabelas, funções e sequências
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO anon, authenticated, service_role;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO anon, authenticated, service_role;
+
+-- Cria o schema '_supavisor' se ele ainda não existir
+CREATE SCHEMA IF NOT EXISTS _supavisor;
